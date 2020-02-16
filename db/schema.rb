c21ce6e72c9fcb265253b9b961e7b77acb45c360
osamu-togo <ku_ri_ka_e_su@i.softbank.jp>
2020-02-17 04:51:31 +0900

    index変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_175423) do
+ActiveRecord::Schema.define(version: 2020_02_15_193219) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_02_14_175423) do
     t.integer "user_id"
     t.integer "row_order"
     t.string "priority"
+    t.integer "task_id"
   end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
